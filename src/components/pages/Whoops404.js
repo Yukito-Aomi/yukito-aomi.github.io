6a2f3c04d92328ead8f7246ef88cf7ac46e9aacc
Yukito-Aomi <yukito.aomi0726@gmail.com>
2025-05-25 14:52:26 +0900

    feat: update Whoops404 component

--- src/components/pages/Whoops404.js
+++ src/components/pages/Whoops404.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 import { Header } from '../Header';
 import { Footer } from '../Footer';
+import './Whoops404.css';
 
 export function Whoops404() {
   let location = useLocation();
@@ -9,8 +10,17 @@ export function Whoops404() {
 
   return (
     <div>
-      <Header />
-      <h1>Resource Not Found at <code>{location.pathname}</code></h1>
+      <div id="whoops404" className="big-bg">
+        <Header />
+        <div className="wrapper">
+          <h2 className="page-title">Resource Not Found</h2>
+        </div>
+      </div>
+      <div className="whoops404-content wrapper">
+        <article>
+          <p>お探しのページ（<code>{location.pathname}</code>）は，移動または削除された削除された可能性があります．</p>
+        </article>
+      </div>
       <Footer />
     </div>
   );
